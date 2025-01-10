663a84b48044999a9ad6aa57d808cd4308dd379f
Sacha Weatherstone <sachaw100@hotmail.com>
2025-01-10 14:35:02 +1000

    restructure & add rust crate publishing

--- packages/ts/mod.ts
+++ packages/ts/mod.ts
@@ -1,2 +1,8 @@
-// Main
-export * as Messages from "./lib/main_pb.ts";
+// Client
+export * as Client from "./lib/client/client_pb.ts";
+
+// Workers
+export * as HydroponicWorker from "./lib/worker/hydroponic_pb.ts";
+export * as IrrigationWorker from "./lib/worker/irrigation_pb.ts";
+export * as WetherWorker from "./lib/worker/weather_pb.ts";
+export * as Worker from "./lib/worker/worker_pb.ts";
