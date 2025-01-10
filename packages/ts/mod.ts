0fdcd3c7e41a0c24daa36aa1c8c243ec4ecbbb94
Sacha Weatherstone <sachaw100@hotmail.com>
2025-01-10 20:44:06 +1000

    standardize worker inventory

--- packages/ts/mod.ts
+++ packages/ts/mod.ts
@@ -4,5 +4,6 @@ export * as Client from "./lib/client/client_pb.ts";
 // Workers
 export * as HydroponicWorker from "./lib/worker/hydroponic_pb.ts";
 export * as IrrigationWorker from "./lib/worker/irrigation_pb.ts";
-export * as WetherWorker from "./lib/worker/weather_pb.ts";
-export * as Worker from "./lib/worker/worker_pb.ts";
+export * as WeatherWorker from "./lib/worker/weather_pb.ts";
+export * as Common from "./lib/worker/common_pb.ts";
+export * as Registration from "./lib/worker/registration_pb.ts";
