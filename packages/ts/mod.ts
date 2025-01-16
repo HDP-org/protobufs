ee9380638d7dc6b04de13f7a8992e42a60d1e588
Sacha Weatherstone <sachaw100@hotmail.com>
2025-01-16 18:54:35 +0800

    streamline protobufs

--- packages/ts/mod.ts
+++ packages/ts/mod.ts
@@ -6,4 +6,3 @@ export * as HydroponicWorker from "./lib/worker/hydroponic_pb.ts";
 export * as IrrigationWorker from "./lib/worker/irrigation_pb.ts";
 export * as WeatherWorker from "./lib/worker/weather_pb.ts";
 export * as Common from "./lib/worker/common_pb.ts";
-export * as Registration from "./lib/worker/registration_pb.ts";
